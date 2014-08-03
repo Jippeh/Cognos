eb54678b410166db18849cc7551aebfbdbd1c5c7
Jippeh <jphs11@gmail.com>
2014-08-03 11:16:30 +0200

    Définition classe Articles

--- model/article.php
+++ model/article.php
@@ -1,1 +1,13 @@
-my class
+<?php
+/*
+// Cognos 2014
+// Classe de définition des articles
+*/
+class Article
+{
+	private $_id;			// Identifiant interne de l'article
+	private $_title;		// Titre de l'article
+	private $_content;		// Contenu de l'article
+	private $_created		// Date de création de l'article
+}
+?>
